f099c52bd53c617261a167f0d1124623bf2f9ae8
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2025-10-28 22:44:14 -0700

    View state mangement: default, cross-section, internal

--- NeuroViz-Unity/Assets/Scripts/BrainStateManager.cs
+++ NeuroViz-Unity/Assets/Scripts/BrainStateManager.cs
@@ -10,6 +10,19 @@ namespace NeuroViz
         public ClickableBrainArea currentSelectedArea;
         public BrainAreaDatabase brainAreaDatabase;
 
+        public GameObject leftCerebrum;
+        public GameObject rightCerebrum;
+        public GameObject cerebellum;
+
+        public enum BrainState
+        {
+            Default = 0,
+            Cross_Section = 1,
+            Internal = 2,
+        }
+
+        [HideInInspector]
+        public BrainState currentState = BrainState.Default;
         private Dictionary<BrainArea, string> brainAreaDescriptions = new Dictionary<BrainArea, string>();
 
         // Singleton implementation
@@ -47,6 +60,32 @@ namespace NeuroViz
                 if (data == null) continue;
                 brainAreaDescriptions.Add(data.brainArea, data.longDescription);
             }
+        }
+
+        public void TransitionToBrainState(int targetStateEnumInt)
+        {
+            BrainState targetState = (BrainState)targetStateEnumInt;
+            switch (targetState)
+            {
+                case BrainState.Default:
+                    rightCerebrum.SetActive(true);
+                    leftCerebrum.SetActive(true);
+                    cerebellum.SetActive(true);
+                    break;
+                case BrainState.Cross_Section:
+                    rightCerebrum.SetActive(false);
+                    leftCerebrum.SetActive(true);
+                    cerebellum.SetActive(true);
+                    break;
+                case BrainState.Internal:
+                    rightCerebrum.SetActive(false);
+                    leftCerebrum.SetActive(false);
+                    cerebellum.SetActive(false);
+                    break;
+                default:
+                    break;
+            }
+            currentState = targetState;
         }
     }
 }
