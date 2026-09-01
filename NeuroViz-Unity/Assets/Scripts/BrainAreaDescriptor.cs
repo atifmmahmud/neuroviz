3b90b15a7ff3b6cf04f9b3a2b4081ee9ac5d76ab
Atif Murtaza Mahmud <atifmahmud95@gmail.com>
2026-09-01 01:46:06 -0700

    Began work on v2

---
+++ NeuroViz-Unity/Assets/Scripts/BrainAreaDescriptor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BrainAreaDescriptor : MonoBehaviour
+{
+    public string Name;
+    public string Description;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NeuroViz-Unity/Assets/Scripts/BrainRegionHierarchies.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class BrainRegionHierarchies : MonoBehaviour
+{
+    public GameObject[] leftCerebralCortex;
+    public GameObject[] rightCerebralCortex;
+    public GameObject[] limbicSystem;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
